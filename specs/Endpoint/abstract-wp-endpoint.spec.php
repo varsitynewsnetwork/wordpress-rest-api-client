53b329ec4eb7e99b651f691f25bffb734eed6e66
Shadi Akiki <shadiakiki1986@gmail.com>
2017-01-03 15:30:17 +0200

    endpoint get able to pass parameters

--- specs/Endpoint/abstract-wp-endpoint.spec.php
+++ specs/Endpoint/abstract-wp-endpoint.spec.php
@@ -34,6 +34,20 @@ describe(AbstractWpEndpoint::class, function () {
             expect($data)->to->equal(['foo' => 'bar']);
         });
 
+        it('should make a GET request with parameters', function () {
+            $client = $this->getProphet()->prophesize(WpClient::class);
+
+            $request = new Request('GET', '/foo?bar=baz');
+            $response = new \GuzzleHttp\Psr7\Response(200, ['Content-Type' => 'application/json'], '{"foo": "bar"}');
+
+            $client->send($request)->willReturn($response)->shouldBeCalled();
+
+            $endpoint = new FakeEndpoint($client->reveal());
+
+            $data = $endpoint->get(null, ['bar'=>'baz']);
+            expect($data)->to->equal(['foo' => 'bar']);
+        });
+
     });
 
     describe('save()', function () {
